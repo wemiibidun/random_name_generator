e7f989beb61a179cbb5dd896348611b329bb39ef
Wemimo Ibidunmoye <wemiibidun@gmail.com>
2021-08-03 16:48:35 -0600

    random_name_generator

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express'); //load express
+const data = require('./MOCK_DATA.json'); //load the mock data json file
+const path = require('path'); //import as a dependency
+
+//console.log(data[0]); how to test if the mock data is properly loaded into app by returning the first name in the array
+
+//how to create a new express app/initialiaztion
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+app.use(express.static('public/assets'));
+
+
+//how to set the app to use
+app.use('/random-name', (req, res) => {
+    const rand_index = Math.round(Math.random() * data.length);
+    const{first_name} = data[rand_index]; //generate random index of array
+    const{last_name } = data[rand_index] //data[Math.round(Math.random() * data.length)];
+    return res.json({first_name, last_name}); //this function will return the properties that are being sent back
+});
+
+app.use('/', (req, res) => {
+    return res.render('index');
+});
+
+app.listen(3000, () => console.log('Listening to the server...'))
